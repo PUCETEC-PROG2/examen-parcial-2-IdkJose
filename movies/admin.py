5970bb41f4931f82ed0574612a42c9430739ef18
José Herrera <jherrerauemee@gmail.com>
2025-01-11 10:29:00 -0500

    Examen Parcial 2

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie, Actor
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'author', 'genre')  # Asegúrate de usar los nombres de campo correctos
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Actor)
+
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100, default='Unknown')
+    author = models.CharField(max_length=100, default='Unknown author')  # Agrega un valor predeterminado
+    year = models.IntegerField()
+    synopsis = models.TextField(default='No synopsis available')
+
+
+    def __str__(self):
+        return self.title
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+
